0df93ec52c9afe48841d33b18dff28c21ffdf148
Gauri Varshney <99128874+Gaurivarshney@users.noreply.github.com>
2024-11-05 15:10:26 +0530

    Update CommentDialog.jsx

--- frnontend/src/components/CommentDialog.jsx
+++ frnontend/src/components/CommentDialog.jsx
@@ -38,7 +38,7 @@ const CommentDialog = ({ open, setOpen }) => {
     const sendMessageHandler = async () => {
       try {
         const res = await axios.post(
-          `http://localhost:8000/api/v1/post/${selectedPost?._id}/comment`,
+          `https://chat-appication-backend.onrender.com/api/v1/post/${selectedPost?._id}/comment`,
           { text },
           {
             headers: {
